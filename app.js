00043eb4e0022d1a055424a90302b438004855fe
Rishav Agarwal <rishav.159@gmail.com>
2016-05-05 10:00:02 +0530

    App running

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
-<<<<<<< HEAD
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
-=======
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -93,7 +56,12 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 module.exports = app;
->>>>>>> 44aac8680000b6bf1f9586d8342e988479ce6714
